3b3de0ac7eb6f50e952ede3c683705f2888081aa
Ville Seppanen <ville.t.seppanen@iki.fi>
2022-08-26 12:01:33 +0300

    Add integration test

---
+++ __tests__/integration/index.test.ts
@@ -0,0 +1,26 @@
+import { SourceClient } from '../../src/datasource/datasource';
+
+describe('Data source', () => {
+  const client = new SourceClient();
+
+  test('should return array of hourly spot prices', async () => {
+    const now = new Date();
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    const startDate = now.toISOString().substring(0,10);
+    const endDate = tomorrow.toISOString().substring(0,10);
+
+    const prices = await client.getSpotPrices(startDate, endDate);
+
+    // Depending on time of day, there should be either only today's or today's and tomorrow's prices
+    expect(prices.length).toBeGreaterThanOrEqual(24);
+
+    for (const hourlyPrice of prices) {
+        expect(hourlyPrice).toHaveProperty('timeStampDay');
+        expect(hourlyPrice).toHaveProperty('timeStampHour');
+        expect(hourlyPrice).toHaveProperty('unit');
+        expect(hourlyPrice).toHaveProperty('value');
+    }
+  });
+
+});
