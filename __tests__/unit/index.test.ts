66165f4f9408e6df41b92dd284ccd59f37e03a8b
Ville Seppanen <ville.t.seppanen@iki.fi>
2025-01-20 22:57:00 +0200

    fix: update price tests

--- __tests__/unit/index.test.ts
+++ __tests__/unit/index.test.ts
@@ -2,12 +2,14 @@ import { getPriceWithFeesAndTaxes } from '../../src/index';
 
 describe('getPriceWithFeesAndTaxes', () => {
   test('should return correct total hourly price', () => {
-    expect(getPriceWithFeesAndTaxes(4.075).toFixed(5)).toEqual("11.16672");
-    expect(getPriceWithFeesAndTaxes(10).toFixed(5)).toEqual("18.51372");
-    expect(getPriceWithFeesAndTaxes(17.37).toFixed(5)).toEqual("27.65252");
-    expect(getPriceWithFeesAndTaxes(35).toFixed(5)).toEqual("49.51372");
-    expect(getPriceWithFeesAndTaxes(12.423).toFixed(2)).toEqual("21.52");
-    expect(getPriceWithFeesAndTaxes(4.123).toFixed(0)).toEqual("11");
+    expect(getPriceWithFeesAndTaxes(4.075).toFixed(5)).toEqual("11.53164");
+    expect(getPriceWithFeesAndTaxes(10).toFixed(5)).toEqual("18.96751");
+
+    expect(getPriceWithFeesAndTaxes(17.37).toFixed(4)).toEqual("28.2169");
+    expect(getPriceWithFeesAndTaxes(35).toFixed(4)).toEqual("50.3425");
+
+    expect(getPriceWithFeesAndTaxes(12.423).toFixed(2)).toEqual("22.01");
+    expect(getPriceWithFeesAndTaxes(4.123).toFixed(0)).toEqual("12");
   });
 
 });
